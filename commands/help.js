f6ad8a6fd79cc195de796fb9471ca331a16fedd1
MGibran <maikgibranr@gmail.com>
2018-09-22 17:38:20 +0700

    Add update-image command

--- commands/help.js
+++ commands/help.js
@@ -9,13 +9,14 @@ module.exports = {
     usage: '',
     execute: async function (message, args) {
         const embed = new RichEmbed();
-        embed.setFooter('Sticker Bot version 1.11')
+        embed.setFooter('Sticker Bot version 1.13')
             .setDescription('To use a sticker, simply type ;stickerkeyword;\nTo see the complete list of available stickers, please visit https://varuzu.azurewebsites.net/')
             .addField('@Stickers#9966 add `[keyword]` `[url or attachment]`', 'Add a new sticker.')
             .addField('@Stickers#9966 search `[query]`', 'Search for stickers keyword that matches the query.')
-            .addField('@Stickers#9966 update-keyword `[keyword]` `[new keyword]`', 'Update a sticker\'s keyword')
-            .addField('@Stickers#9966 update-url `[keyword]` `[url]`', 'Update a sticker\'s image url')
-            .addField('@Stickers#9966 help', 'Shows this message');
+            .addField('@Stickers#9966 update-keyword `[keyword]` `[new keyword]`', 'Update a sticker\'s keyword.')
+            .addField('@Stickers#9966 update-url `[keyword]` `[url]`', 'Update a sticker\'s image using an url.')
+            .addField('@Stickers#9966 update-image `[keyword]` `[attachment]`', 'Update a sticker\'s image using an image attachment.')
+            .addField('@Stickers#9966 help', 'Shows this message.');
 
         message.channel.send({embed: embed});
     }
---
+++ commands/update-image.js
@@ -0,0 +1,35 @@
+const apiService = require('../services/api-service')
+const feature = require('../features/stickers');
+
+module.exports = {
+    name: 'update-image',
+    args: true,
+    ownerOnly: false,
+    cooldown: 3,
+    sortIndex: 0,
+    usage: '[keyword]',
+    optional: '[attachment]',
+    execute: async function (message, args) {
+        const keyword = args[0];
+        const attachment = message.attachments.first();
+        let url = attachment.url;
+
+        try {
+            const sticker = await apiService.get(keyword);
+            const newSticker = {
+                keyword: sticker.keyword,
+                url: url,
+                useCount: sticker.useCount,
+                upload: {
+                    id: sticker.upload.id,
+                    username: sticker.upload.username
+                }
+            }
+            const result = await apiService.update(sticker.id, newSticker);
+            feature.refresh();
+            return message.reply(`Updated ${sticker.keyword} image.`);
+        } catch (error) {
+            return message.reply(error.error.message);
+        }
+    }
+};
